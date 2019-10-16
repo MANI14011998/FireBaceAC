5b347d3a33495b1c5a3b910811eed51b4726b7c1
mani14011998 <mani14011998@gmail.com>
2019-10-16 13:36:26 +0530

    project

--- chatapp/ViewController.swift
+++ chatapp/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var myimageview: UIImageView!
+    @IBOutlet weak var txtText: UITextField!
+    var ref = DatabaseReference.init()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.ref = Database.database().reference()
+       let tapGesture = UITapGestureRecognizer()
+        myimageview.isUserInteractionEnabled = true
+        myimageview.addGestureRecognizer(tapGesture)
+        
+        
+        
+    }
+    
+    @obj
+    
+    @IBAction func btnsaveclick(_ sender: UIButton) {
+           self.saverFIRData()
+    }
+    func saverFIRData(){
+            let dict = ["name": "ankita", "text": txtText.text!]
+            self.ref.child("chat").childByAutoId().setValue(dict)
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
-}
+
 
